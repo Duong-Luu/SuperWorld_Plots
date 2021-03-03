7389c7ae05a3f9625c3a1790801e6c86de99dde3
Duong Luu <onlyduong@gmail.com>
2021-03-02 20:52:30 -0800

    plot auto-commit

--- commit.bat
+++ commit.bat
@@ -1,8 +1,8 @@
-"C:\Users\Tziporah\anaconda3\python.exe" "C:\Users\Tziporah\SuperWorld_Plot_Recommendation\data\SuperWorld_Plots_Sold.py"
+"C:\Users\onlyd\AppData\Local\Continuum\anaconda3\python.exe" "C:\Users\onlyd\Desktop\SW_plot\data\SuperWorld_Plots_Sold.py"
 
 Rscript -e "rmarkdown::render('C:\\Users\\Tziporah\\SuperWorld_Plots\\index.Rmd')"
 
-cd C:\\Users\\Tziporah\\SuperWorld_Plots
+cd C:\Users\onlyd\Desktop\SuperWorld_plot_Dashboard\SuperWorld_Plots
 
 git add .
 git commit -m "plot auto-commit"
